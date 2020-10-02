acee03a6ee0676c17a86d02df35c9687f26a0da2
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2020-10-02 12:18:34 +0900

    num2kanzi_first

--- www/num2kan.py
+++ www/num2kan.py
@@ -5,6 +5,7 @@ import importlib
 import datetime
 from datetime import timedelta
 def n2k_simo(input_number):
+    input_number=int(input_number)
     if input_number==1:
         return "壱"
     if input_number==2:
@@ -24,15 +25,37 @@ def n2k_simo(input_number):
     if input_number==9:
         return "九"
     return "零"
+def n2k_naka(input_number):
+    input_number=int(input_number)
+    return_kanzi=""
+    if input_number//1000>0:
+        return_kanzi+=n2k_simo(input_number//1000)+"千"
+        input_number%=1000
+    if input_number//100>0:
+        return_kanzi+=n2k_simo(input_number//100)+"百"
+        input_number%=100
+    if input_number//10>0:
+        return_kanzi+=n2k_simo(input_number//10)+"捨"
+        input_number%=10
+    if input_number//1>0:
+        return_kanzi+=n2k_simo(input_number//1)
+    return return_kanzi
 def n2k(input_number):
     input_number=int(input_number)
     return_kanzi=""
+    if input_number==0:
+        return "零"
     if input_number//1000000000000>0:
-        return_kanzi+=n2k_simo(input_number//1000000000000)+"兆"
+        return_kanzi+=n2k_naka(input_number//1000000000000)+"兆"
+        input_number%=1000000000000
     if input_number//100000000>0:
-        return_kanzi+=n2k_simo(input_number//100000000)+"億"
+        return_kanzi+=n2k_naka(input_number//100000000)+"億"
+        input_number%=100000000
     if input_number//10000>0:
-        return_kanzi+=n2k_simo(input_number//10000)+"万"
+        return_kanzi+=n2k_naka(input_number//10000)+"万"
+        input_number%=10000
+    if input_number//1>0:
+        return_kanzi+=n2k_naka(input_number//1)
     return return_kanzi
 def show(value):
     try:
